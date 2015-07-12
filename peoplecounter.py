c7720c620e932491247e441558937450e12a1204
Troy Ross <kak.bo.che@gmail.com>
2015-07-12 01:13:41 -0600

    adding peoplecounter.py to repo

---
+++ peoplecounter.py
@@ -0,0 +1,81 @@
+from ubidots import ApiClient
+import RPi.GPIO as GPIO
+import time
+import socket
+import yaml # pyyaml
+import argparse
+import sys
+
+class PeopleCounter(object):
+    def __init__(self, configuration_file=''):
+        self.config = {}
+        options = self._parseOptions()
+        configuration_file = options.configuration_file
+        self._loadConfig(configuration_file)
+        self._ubidotsConnect()
+        self.ipaddress = socket.gethostbyname(socket.gethostname())
+        self._configureSensors()
+        self.ubidotsConfig()
+
+    def _parseOptions(self):
+        parser = argparse.ArgumentParser(description='Monitor people entering space')
+        parser.add_argument('configuration_file', help='YAML configuration File')
+        return parser.parse_args()
+
+    def _loadConfig(self, filename):
+        try:
+            f = open(filename)
+            # use safe_load instead load
+            configuration = yaml.safe_load(f)
+            f.close()
+            self.api_key = configuration['api_key']
+            self.sensors = configuration['sensors']
+
+        except yaml.scanner.ScannerError, e:
+            print "ERROR: You likely have no space after a colon in your config file"
+            sys.exit(1)
+
+    def _configureSensors(self):
+        GPIO.setmode(GPIO.BCM)
+        for sensor_name, sensor_config in self.sensors.iteritems():
+            GPIO.setup(sensor_config['gpio'], GPIO.IN)
+            sensor_config['last_presense'] = 0
+            if not 'count' in sensor_config:
+                sensor_config['count'] = 0
+
+    def ubidotsConfig(self):
+        for tries in range(0,10):
+            try:
+                self._ubidotsConnect()
+                self._ubidotsRegisterSensors()
+                return True
+            except requests.exceptions.ConnectionError:
+                time.sleep(10)
+
+        print "Failed to connect to UbiDots"
+        sys.exit(1)
+
+    def _ubidotsConnect(self):
+        self.api = ApiClient(self.api_key)
+
+    def _ubidotsRegisterSensors(self):
+        for sensor_name, sensor_config in self.sensors.iteritems():
+            self.sensors[sensor_name]['ref'] = self.api.get_variable(sensor_config['id'])
+
+    def monitorSensors(self):
+        while(True):
+            try:
+                for sensor_name, sensor_config in self.sensors.iteritems():
+                    presence = GPIO.input(sensor_config['gpio'])
+                    if(presence != sensor_config['last_presense']):
+                        if presence == 1:
+                            sensor_config['count'] += 1
+                            print "%s: %d" %(sensor_name, sensor_config['count'])
+                            sensor_config['ref'].save_value({'value':sensor_config['count'], 'context':{'ipaddress':self.ipaddress}})
+                        sensor_config['last_presense'] = presence
+                time.sleep(0.25)
+            except requests.exceptions.ConnectionError:
+                self.ubidotsConfig()
+
+counter = PeopleCounter()
+counter.monitorSensors()
